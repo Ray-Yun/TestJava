4dd270b2f3028d7fd53f32ba5a58d04e39900bd6
Ray <rays20715@gmail.com>
2020-09-01 10:26:30 +0800

    Java Practice

---
+++ src/controller/TestMain.java
@@ -0,0 +1,11 @@
+package controller;
+
+import java.util.Scanner;
+
+public class TestMain {
+	public static void main(String[] args ) {
+		Scanner iput= new Scanner(System.in);
+		System.out.println("請輸入一個整數：");
+		System.out.println(iput.nextLine());
+	}
+}
---
+++ src/service/TestService.java
@@ -0,0 +1,14 @@
+package service;
+
+public class TestService {
+	public TestService() { //宣告 constructor
+		int a,b,c,d,e,f,g,h,i,j; //宣告10個型態為int的變數
+
+	}
+	
+	public static void main(String[] args) {
+		  TestService service = new TestService();
+		  
+		 
+		}
+}
